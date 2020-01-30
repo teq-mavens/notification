26979056b92346422e1adbb2738b13e11f767594
yadav-pooja-ji <54525035+yadav-pooja-ji@users.noreply.github.com>
2020-01-30 15:54:12 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,27 +1,27 @@
 /*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
+ *
+ *  Push Notifications codelab
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
 
 /* eslint-env browser, es6 */
 
 'use strict';
 
-const applicationServerPublicKey ='<YOUR_PUBLIC_VAPID_KEY>';
+const applicationServerPublicKey = '<YOUR_PUBLIC_VAPID_KEY>';
 
 let isSubscribed = false;
 let isSubscribedDelay = false;
@@ -31,119 +31,119 @@ let swRegistration = null;
  *-- convert base 64 string to uint8array
  *>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 function urlB64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
+	const padding = '='.repeat((4 - base64String.length % 4) % 4);
+	const base64 = (base64String + padding)
+		.replace(/\-/g, '+')
+		.replace(/_/g, '/');
 
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
+	const rawData = window.atob(base64);
+	const outputArray = new Uint8Array(rawData.length);
 
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
+	for (let i = 0; i < rawData.length; ++i) {
+		outputArray[i] = rawData.charCodeAt(i);
+	}
+	return outputArray;
 }
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  *-- register service worker on browser and check if push is supported 
  *>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
- $(document).ready(function() {
-   if ('serviceWorker' in navigator && 'PushManager' in window) {
-      //if(process.env.NODE_ENV === 'production'){
-      console.log('Service Worker and Push is supported');
-      navigator.serviceWorker.register("/sw.js")
-      .then(function(swReg) {
-        
-        console.log('Service Worker is registered', swReg);
-        swRegistration = swReg;
-        initializeUI();
-      })
-      .catch(function(error) {
-        console.error('Service Worker Error', error);
-      });
-  } else {
-    console.warn('Push messaging is not supported');    
-  }
+$(document).ready(function () {
+	if ('serviceWorker' in navigator && 'PushManager' in window) {
+		//if(process.env.NODE_ENV === 'production'){
+		console.log('Service Worker and Push is supported');
+		navigator.serviceWorker.register("/sw.js")
+			.then(function (swReg) {
+
+				console.log('Service Worker is registered', swReg);
+				swRegistration = swReg;
+				initializeUI();
+			})
+			.catch(function (error) {
+				console.error('Service Worker Error', error);
+			});
+	} else {
+		console.warn('Push messaging is not supported');
+	}
 });
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  *-- click event to check if the user is currently subscribed
  *>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 function initializeUI() {
-    // trigger push notification on button click
-      if (isSubscribed) {
-        unsubscribeUser();
-      } else {      
-        subscribeUser();      
-      }
-     // Set the initial subscription value
-     swRegistration.pushManager.getSubscription()
-    .then(function(subscription) {
-        isSubscribed = !(subscription === null);
-        isSubscribedDelay = !(subscription === null);
+	// trigger push notification on button click
+	if (isSubscribed) {
+		unsubscribeUser();
+	} else {
+		subscribeUser();
+	}
+	// Set the initial subscription value
+	swRegistration.pushManager.getSubscription()
+		.then(function (subscription) {
+			isSubscribed = !(subscription === null);
+			isSubscribedDelay = !(subscription === null);
 
-        if (isSubscribed) {
-          console.log('User IS subscribed.');
-        } else {
-          console.log('User is NOT subscribed.');
-        }
-    });
+			if (isSubscribed) {
+				console.log('User IS subscribed.');
+			} else {
+				console.log('User is NOT subscribed.');
+			}
+		});
 }
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  *-- if User is subscribed send then push notification 
  *>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 function subscribeUser() {
 
-  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
-  swRegistration.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: applicationServerKey
-  })
-  .then(function(subscription) {
-    
-    console.log('User is subscribed.');
-    // do your stuff here
-    updateSubscriptionOnServer(subscription);
-    //send push notification
-    fetch('/webpush',{
-      method:'POST',
-      body: JSON.stringify(subscription),
-      headers:{
-        'content-type': 'application/json'
-      }
-    });  
-    isSubscribed = true;  
+	const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+	swRegistration.pushManager.subscribe({
+			userVisibleOnly: true,
+			applicationServerKey: applicationServerKey
+		})
+		.then(function (subscription) {
 
-  })
-  .catch(function(err) {
-    console.log('Failed to subscribe the user: ', err);    
-  });
+			console.log('User is subscribed.');
+			// do your stuff here
+			updateSubscriptionOnServer(subscription);
+			//send push notification
+			fetch('/webpush', {
+				method: 'POST',
+				body: JSON.stringify(subscription),
+				headers: {
+					'content-type': 'application/json'
+				}
+			});
+			isSubscribed = true;
+
+		})
+		.catch(function (err) {
+			console.log('Failed to subscribe the user: ', err);
+		});
 }
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  *-- UNsubscribe Users
  *>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 function unsubscribeUser() {
-  swRegistration.pushManager.getSubscription()
-  .then(function(subscription) {
-    if (subscription) {
-      return subscription.unsubscribe();
-    }
-  })
-  .catch(function(error) {
-    console.log('Error unsubscribing', error);
-  })
-  .then(function() {
-    updateSubscriptionOnServer(null);
-    console.log('User is unsubscribed.');
-    isSubscribed = false;
-  });
+	swRegistration.pushManager.getSubscription()
+		.then(function (subscription) {
+			if (subscription) {
+				return subscription.unsubscribe();
+			}
+		})
+		.catch(function (error) {
+			console.log('Error unsubscribing', error);
+		})
+		.then(function () {
+			updateSubscriptionOnServer(null);
+			console.log('User is unsubscribed.');
+			isSubscribed = false;
+		});
 }
 
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  *-- function to send your subscription data to a backend
  *>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 function updateSubscriptionOnServer(subscription) {
-  // TODO: Send subscription to application server
-  const subscriptionJson = document.querySelector('.js-subscription-json');
-  const subscriptionDetails = document.querySelector('.js-subscription-details');
+	// TODO: Send subscription to application server
+	const subscriptionJson = document.querySelector('.js-subscription-json');
+	const subscriptionDetails = document.querySelector('.js-subscription-details');
 
 }
