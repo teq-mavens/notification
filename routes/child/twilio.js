8fe1954f22fc8c83b14fa88fdd52e337cbacccdb
yadav-pooja-ji <54525035+yadav-pooja-ji@users.noreply.github.com>
2020-01-30 15:50:34 +0530

    Update twilio.js

--- routes/child/twilio.js
+++ routes/child/twilio.js
@@ -1,13 +1,13 @@
-process.on('message', function(m) {
+process.on('message', function (m) {
 	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.//
-	var mysql       = require('mysql');
-  	var credentials;
-	try{
-	    credentials = require('../../config/db_credentials'); //CREATE THIS FILE YOURSELF
-	}catch(e){
-	    credentials = require('../../config/db_credentials_env');
+	var mysql = require('mysql');
+	var credentials;
+	try {
+		credentials = require('../../config/db_credentials'); //CREATE THIS FILE YOURSELF
+	} catch (e) {
+		credentials = require('../../config/db_credentials_env');
 	}
-	var connection  = mysql.createConnection(credentials);
+	var connection = mysql.createConnection(credentials);
 	connection.connect();
 	var twilio_credentials = require('../../config/twilio_credentials');
 	var accountSid = twilio_credentials.accountSid;
@@ -15,50 +15,50 @@ process.on('message', function(m) {
 	var twilio = require('twilio');
 	var client = new twilio(accountSid, authToken);
 	var MessagingResponse = require('twilio').twiml.MessagingResponse;
-  	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.//
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.//
 
 	console.log('sms twilio CHILD got message:', m);
-	try{
+	try {
 		client.messages.create({
-		    to: '+'+m.required_body.to_country_code+m.required_body.to_number,	
-		    from: '+'+m.required_body.from_country_code+m.required_body.from_number, 
-		    body: m.required_body.message
-		}, function(error, message) {
-		    if (!error) {
-		        var message_json = JSON.stringify(message);
-		      	var status = (message.status)? message.status: '';
-		      	
-		      	let sql = 'INSERT INTO `push_logs`(`push_type`, `reference_key`, `status`, `json_respone`) VALUES (?,?,?,?)';
-				let params = [m.push_type,m.reference_key,status,message_json];
-				connection.query(sql,params, (err, rows, fields) => {
-			        if (err){
-			        	throw err;
-			        } else{
-			        	console.log('sms twilio success log successfully inserted in database');
-			        }					        
-			    });
-		    } else {
-		        let sql = 'INSERT INTO `push_logs`(`push_type`, `reference_key`, `status`, `json_respone`) VALUES (?,?,?,?)';
-				let params = [m.push_type,m.reference_key,'error',error];
-				connection.query(sql,params, (err, rows, fields) => {
-			        if (err){
-			        	throw err;
-			        } else{
-			        	console.log('sms twilio error log successfully inserted in database');
-			        }					        
-			    });
-		    }
+			to: '+' + m.required_body.to_country_code + m.required_body.to_number,
+			from: '+' + m.required_body.from_country_code + m.required_body.from_number,
+			body: m.required_body.message
+		}, function (error, message) {
+			if (!error) {
+				var message_json = JSON.stringify(message);
+				var status = (message.status) ? message.status : '';
+
+				let sql = 'INSERT INTO `push_logs`(`push_type`, `reference_key`, `status`, `json_respone`) VALUES (?,?,?,?)';
+				let params = [m.push_type, m.reference_key, status, message_json];
+				connection.query(sql, params, (err, rows, fields) => {
+					if (err) {
+						throw err;
+					} else {
+						console.log('sms twilio success log successfully inserted in database');
+					}
+				});
+			} else {
+				let sql = 'INSERT INTO `push_logs`(`push_type`, `reference_key`, `status`, `json_respone`) VALUES (?,?,?,?)';
+				let params = [m.push_type, m.reference_key, 'error', error];
+				connection.query(sql, params, (err, rows, fields) => {
+					if (err) {
+						throw err;
+					} else {
+						console.log('sms twilio error log successfully inserted in database');
+					}
+				});
+			}
 		});
-	}catch(ex){
+	} catch (ex) {
 		let sql = 'INSERT INTO `push_logs`(`push_type`, `reference_key`, `status`, `json_respone`) VALUES (?,?,?,?)';
-		let params = [m.push_type,m.reference_key,'error',ex];
-		connection.query(sql,params, (err, rows, fields) => {
-	        if (err){
-	        	throw err;
-	        } else{
-	        	console.log('sms twilio error log successfully inserted in database');
-	        }					        
-	    });
+		let params = [m.push_type, m.reference_key, 'error', ex];
+		connection.query(sql, params, (err, rows, fields) => {
+			if (err) {
+				throw err;
+			} else {
+				console.log('sms twilio error log successfully inserted in database');
+			}
+		});
 	}
 
 });
