5f940a25a7e31863061e8b0a9020905306f591a6
yadav-pooja-ji <54525035+yadav-pooja-ji@users.noreply.github.com>
2020-01-30 15:57:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,29 +2,31 @@
  *-- created on : 24th dec 2019
  *-- main app.js file
  *--<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
-var express     = require('express');
-var app         = express();
-var path        = require('path');
-var bodyParser  = require('body-parser');
-var mysql       = require('mysql');
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var credentials;
-try{
-    credentials = require('./config/db_credentials'); //CREATE THIS FILE YOURSELF
-}catch(e){
-    //heroku support
-    credentials = require('./config/db_credentials_env');
+try {
+	credentials = require('./config/db_credentials'); //CREATE THIS FILE YOURSELF
+} catch (e) {
+	//heroku support
+	credentials = require('./config/db_credentials_env');
 }
 // Setup MySQL Connection
-var connection  = mysql.createConnection(credentials);
+var connection = mysql.createConnection(credentials);
 // // Connect to MySQL DB
 connection.connect();
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
-     
+
 // instruct express to server up static assets
 app.use(express.static('public'));
 
@@ -41,9 +43,9 @@ app.use('/webpush', webpushRouter);
 app.use('/notification', pushRouter);
 // non api route for our views
 app.get('/', (req, res) => {
-    //res.render('index');
-    console.log("HELLO EVERYONE");
-    res.sendFile( __dirname + "/" + "public/index.html" );
+	//res.render('index');
+	console.log("HELLO EVERYONE");
+	res.sendFile(__dirname + "/" + "public/index.html");
 });
 // Better way to disable x-powered-by
 app.disable('x-powered-by');
@@ -57,4 +59,3 @@ var port        = process.env.PORT || 3000;
 var host        = process.env.HOST || '0.0.0.0'; // For Heroku
 app.listen(port, host);
 console.log('server is running');*/
-
